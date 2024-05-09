c3d99f62fccae43e1c22fed106e0f98076c05af5
JamesMeadows4 <162722724+JamesMeadows4@users.noreply.github.com>
2024-05-09 17:48:51 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 r = int(input("Enter the red: "))
 g = int(input("Enter the green: "))
 b = int(input("Enter the blue: "))
-if r > 255 or g > 255 or b > 255:
-    if r > 255:
+if r > 255 or g > 255 or b > 255 or r < 0 or g < 0 or b < 0:
+    if r > 255 or r < 0:
         print("Red number is not correct")
-    if g > 255:
+    if g > 255 or g < 0:
         print("Green number is not correct")
-    if b > 255:
+    if b > 255 or b < 0:
         print("Blue number is not correct")
 else:
     print("No problems found!")
